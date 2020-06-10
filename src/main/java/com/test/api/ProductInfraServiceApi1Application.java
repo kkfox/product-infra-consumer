89b6105bc6ca07300392ec9a7b5d659a798cbd9a
kevinlee <kevinlee@ms26.url.com.tw>
2020-06-10 17:01:43 +0800

    'initial'

---
+++ src/main/java/com/test/api/ProductInfraServiceApi1Application.java
@@ -0,0 +1,13 @@
+package com.test.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductInfraServiceApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductInfraServiceApi1Application.class, args);
+	}
+
+}
